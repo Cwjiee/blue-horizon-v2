01b7b9b4b5628573fddaceb0af2dbc3dcdb49292
Cwjiee <jackchong398@gmail.com>
2023-09-16 23:00:58 +0800

    fix: minor bug fixes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,21 +5,21 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <div class="mx-auto mt-20">
-        <footer class="p-4 rounded-lg shadow md:px-6 md:py-8">
-          <div class="sm:flex sm:items-center sm:justify-between">
+      <div className="mx-auto mt-20">
+        <footer className="p-4 rounded-lg shadow md:px-6 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
             <a
               href="https://www.padi.com/"
               target="_blank"
-              class="flex items-center mb-4 sm:mb-0"
+              className="flex items-center mb-4 sm:mb-0"
             >
               <Image src={Padi} alt="padi-logo" />
             </a>
-            <ul class="flex flex-wrap items-center mb-6 sm:mb-0">
+            <ul className="flex flex-wrap items-center mb-6 sm:mb-0">
               <li>
                 <Link
                   href="/courses"
-                  class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+                  className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
                 >
                   courses
                 </Link>
@@ -27,7 +27,7 @@ export default function Footer() {
               <li>
                 <Link
                   href="/about"
-                  class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+                  className="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
                 >
                   About
                 </Link>
@@ -35,15 +35,15 @@ export default function Footer() {
               <li>
                 <Link
                   href="/contact"
-                  class="text-sm text-gray-500 hover:underline dark:text-gray-400"
+                  className="text-sm text-gray-500 hover:underline dark:text-gray-400"
                 >
                   Contact
                 </Link>
               </li>
             </ul>
           </div>
-          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023 Blue Horizon. All Rights Reserved.
           </span>
         </footer>
